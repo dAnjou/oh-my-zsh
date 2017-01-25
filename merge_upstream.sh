660b30af3aee232820c5aeeb48aff957059690e2
dAnjou <mail@danjou.de>
2017-01-25 23:31:39 +0100

    improved merge_upstream.sh

--- merge_upstream.sh
+++ merge_upstream.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git remote add upstream https://github.com/robbyrussell/oh-my-zsh.git
 git fetch upstream &&
 git checkout master &&
-git merge upstream/master
+git merge upstream/master &&
+git push origin master
